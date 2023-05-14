3a2677d08abf03c4b4c39c20506792509a931dd5
Gabriel Maphanga <gabrielmaphang@gmail.com>
2023-05-14 23:21:01 +0200

    DBupdate

---
+++ TaxCalculator.Infrastructure/Create dbo.CalculatedAnnualTax.sql
@@ -0,0 +1,18 @@
+﻿USE [TaxCalculator]
+GO
+
+/****** Object: Table [dbo].[CalculatedAnnualTax] Script Date: 14 May 2023 23:16:48 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[CalculatedAnnualTax] (
+    [Id]            INT      IDENTITY (1, 1) NOT NULL,
+    [CalculatedTax] INT      NOT NULL,
+    [NetPay]        INT      NOT NULL,
+    [DateTime]      DATETIME NOT NULL
+);
+
+
--- TaxCalculator.Infrastructure/dbo.Table_1.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[CalculatedAnnualTax]
-(
-	[Id] INT NOT NULL PRIMARY KEY,
-	[CalculatedTax] [int] NOT NULL,
-	[NetPay] [int] NOT NULL,
-	[DateTime] [datetime] NOT NULL
-)
